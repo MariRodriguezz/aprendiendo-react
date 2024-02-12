d16e9ce3f62e9f01c01960ee5efed95494071dc4
MariRodriguezz <mariarodriguezgomez678@gmail.com>
2024-02-12 12:25:17 +0100

    primer commit

---
+++ 00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,20 @@
+
+export function TwitterFollowCard(){
+    return(
+       <article className='tw-followCard'>
+        <header className='tw-followCard-header'>
+            <img className='tw-followCard-avatar'
+             alt="Mi avatar de twitter" src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4"></img>
+        <div className='tw-followCard-info'>
+            <strong>Mari Rodriguez</strong>
+            <span className='tw-followCard-infoUserName'>@marirodriguez</span>
+        </div>
+        </header>
+        <aside>
+            <button className='tw-followCard-button'>
+                Seguir
+            </button>
+        </aside>
+       </article>
+    )
+}
